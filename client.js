a9ffda2b9cca19b5dbaa56b596223c679e411006
edwardm8 <edwardm825@gmail.com>
2023-10-06 09:59:40 -0600

    finished content

--- client.js
+++ client.js
@@ -1,1 +1,32 @@
 const myList = ["Pizza", "Burger", "Pasta", "Fried Chicken"]
+const nextList = ["Noodles" , "Sushi"]
+console.log(myList)
+console.log(nextList)
+
+/* add to back of the list*/
+myList.push("Hotdog")
+console.log(myList)
+
+/*remove item from back of list*/
+myList.pop(myList)
+console.log(myList)
+
+/*combine the two lists*/
+const combineList = myList.concat(nextList)
+console.log(combineList)
+
+const pizza = {size:"large", sauce:"tomato", topping:"pepperoni"}
+console.log(pizza)
+
+/*values in array*/
+const valueList = Object.values(pizza)
+console.log(valueList)
+
+/*keys in array*/
+const keyList = Object.keys(pizza)
+console.log(keyList)
+
+/*nested array*/
+const nestArray = Object.entries(pizza)
+console.log(nestArray)
+
